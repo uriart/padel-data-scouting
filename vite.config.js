a52f620e5166d7721586de64e1a2f437f70a0a81
DESKTOP-IM9483I\Uri <loiro_8@hotmail.com>
2025-08-28 13:46:05 +0200

    Fix redirects

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: "_redirects", dest: "" }
+      ]
+    })
+  ],
   server: {
     open: true
   }
